5e379e5d5f2b101f9a289e0e6f951014d15f8b48
harshKumar029 <harshanand029@gmail.com>
2024-10-04 03:23:02 +0530

    dep

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Make sure this matches your platform's publish directory
+  },
+  server: {
+    historyApiFallback: true, // This ensures correct routing for Vite dev server
+  }
+});
